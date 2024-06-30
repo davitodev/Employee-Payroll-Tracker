4830cd466142ef67d6469c0e790c8927ae409a6c
davitodev <dvd_barren@yahoo.com>
2024-06-29 20:18:06 -0500

    Remove erroneous console logs

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
   var sum = 0;
   for(i = 0; i < employeesArray.length; i++){
-    console.log(`the salary is; ${employeesArray[i].salary}`)
     sum = sum + employeesArray[i].salary
   }
   console.log(sum / employeesArray.length) 
